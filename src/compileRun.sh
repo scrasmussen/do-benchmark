0d1f1eba7ce4530d69ab064c44eaaba11f13b1a8
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-01-31 17:14:10 +0000

    autosave

--- src/compileRun.sh
+++ src/compileRun.sh
@@ -10,23 +10,27 @@ function run {
 
 
 export TAU_OPTIONS='-optNoMPI -optPreProcess'
-export TAU_OPTIONS='-optNoMPI -optPreProcess -optKeepfiles'
 export TAU_MAKEFILE=/apps/software/TAU/2.27-foss-2018a/x86_64/lib/Makefile.tau-papi-pdt-openmp-opari
+file=saxpy
+echo "-----------------------omp num threads = ${OMP_NUM_THREADS}"
 
 rm -f output.txt
 echo ";SAXBY;SAXBY_DO;SAXBY_DO_CONCURRENT" >> output.txt
-for (( i=1; i<=1000; i+=100 ))
-# for (( i=1; i<=1050000; i+=10000 ))
+for (( i=1000; i<=10000; i+=1000000 ))
+# for (( i=1; i<=4000000; i+=50000 ))
 do
     if [ $i -eq 1 ]; then
-	let n=1
+    	let n=1
     else
-	let n=$i-1
+    	let n=$i-1
     fi
-    cmd="tau_f90.sh -g -O3 -DSETN=${n} -DSETRUN=1000 saxpy.F90 -o saxpy.exe"
+
+    cmd="tau_f90.sh -g -O3 -DSETN=${n} -DSETRUN=1000 ${file}.F90 -o ${file}.exe"
+    cmd="tau_f90.sh -g -O3 -DSETN=${n} -DSETRUN=1 ${file}.F90  -o ${file}.exe"
     run $cmd
-    run "./saxpy.exe"
+    run "./${file}.exe"
     printf "${n};" >> output.txt
     run "./extractInfo.awk profile.0.0.0"
+    rm profile.0.0.0
 done
 echo "FIN"
--- src/saxpy.F90
+++ src/saxpy.F90
@@ -9,18 +9,6 @@ subroutine saxpy_do_concurrent(X,Y,a,n)
   end do
 end subroutine saxpy_do_concurrent
 
-subroutine saxpy_do_omp(X,Y,a,n)
-  implicit none
-  real, intent(IN) :: X(n), a
-  real, intent(INOUT) :: Y(n)
-  integer, intent(IN) :: n
-  integer :: i
-
-  do i=1,n
-     Y(i) = a*X(i) + Y(i)
-  end do
-end subroutine saxpy_do_omp
-
 subroutine saxpy_do(X,Y,a,n)
   implicit none
   real, intent(IN) :: X(n), a
@@ -44,16 +32,16 @@ end subroutine saxpy
 
 program run_saxpy
   implicit none
-  integer, parameter :: N=400000, RUN=1000
+  integer, parameter :: n=SETN, RUN=SETRUN
   integer :: i
-  real, dimension(N) :: X,Y
+  real, dimension(n) :: X,Y
   real :: a
 
+  print *, "===START PROGRAM==="
   ! In Fortran 2018, not yet implemented
   ! call random_init(.true.,.true.)
   call random_seed()
   call random_number(a)
-
 
   do i=1,RUN
      call random_number(X)
@@ -68,6 +56,5 @@ program run_saxpy
      call random_number(Y)
      call saxpy_do_concurrent(X,Y,a,n)
   end do
-
-  print *, "Fin"
+  print *, "===Fin==="
 end program run_saxpy
