17f76278a07deacb9aac5752af5cedac12ab422b
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-02-10 14:43:16 +0000

    slowly moving things over to run from the command line

--- src/compileRunOMP.sh
+++ src/compileRunOMP.sh
@@ -8,58 +8,57 @@ function run {
     fi
 }
 
+function program {
+    echo "ARTLESS"
+    exit 1
+    fflag="-g ${optimization}"
+    output=outputSaxpy${optimization}.txt
+    rm -f ${output}
+    echo  "tau_f90.sh ${fflag} -DSETN=n -DSETRUN=10000 ${file}.F90 -o ${file}.exe" >> ${output}
+    echo ";SAXPY;SAXPY_DO;SAXPY_DO_CONCURRENT;SAXPY_DO_OMP" >> ${output}
+    # for (( i=1; i<=4000000; i+=50000 ))
+	for (( i=1; i<=4; i+=50000 ))
+    do
+	if [ $i -eq 1 ]; then
+    	    let n=1
+	else
+    	    let n=$i-1
+	fi
 
+	cmd="tau_f90.sh ${fflag} -DSETN=${n} -DSETRUN=10000 ${file}.F90 -o ${file}.exe"
+	run $cmd
+	run "./${file}.exe"
+	printf "${n};" >> ${output}
+	run "./extractInfoOMP.awk profile.0.0.0 ${output}"
+	run "rm -f ${file}.exe"
+    done
+}
+
+
+# set TAU OPTIONS and file name
 export TAU_OPTIONS='-optNoMPI -optPreProcess -optContinueBeforeOMP'
 export TAU_MAKEFILE=/apps/software/TAU/2.27-foss-2018a/x86_64/lib/Makefile.tau-papi-pdt-openmp-opari
-file=saxpy_omp
-echo "-----------------------omp num threads = ${OMP_NUM_THREADS}"
 
-optimization="-O0"
-fflag="-g ${optimization}"
-output=outputSaxpy${optimization}.txt
-rm -f ${output}
-echo  "tau_f90.sh ${fflag} -DSETN=n -DSETRUN=10000 ${file}.F90 -o ${file}.exe" >> ${output}
-echo ";SAXPY;SAXPY_DO;SAXPY_DO_CONCURRENT;SAXPY_DO_OMP" >> ${output}
-for (( i=1; i<=4000000; i+=50000 ))
-# for (( i=1; i<=4; i+=50000 ))
-do
-    if [ $i -eq 1 ]; then
-    	let n=1
-    else
-    	let n=$i-1
-    fi
-
-    cmd="tau_f90.sh ${fflag} -DSETN=${n} -DSETRUN=10000 ${file}.F90 -o ${file}.exe"
-    run $cmd
-    run "./${file}.exe"
-    printf "${n};" >> ${output}
-    run "./extractInfoOMP.awk profile.0.0.0 ${output}"
-    run "rm -f ${file}.exe"
+# parse command line arguments for optimization level
+for arg in "$@"; do
+    case $arg in
+	-file=*)
+	    file=${arg#"-file="}
+	    ;;
+	-O0|-O1|-O2|-O3)
+	    optimization=$arg
+	    ;;
+	*)
+	    echo $arg "not an accepted argument"
+	    exit 1
+	    ;;
+    esac
 done
 
-
-optimization="-O3"
-fflag="-g ${optimization}"
-output=outputSaxpy${optimization}.txt
-rm -f ${output}
-echo  "tau_f90.sh ${fflag} -DSETN=n -DSETRUN=10000 ${file}.F90 -o ${file}.exe" >> ${output}
-echo ";SAXPY;SAXPY_DO;SAXPY_DO_CONCURRENT;SAXPY_DO_OMP" >> ${output}
-for (( i=1; i<=4000000; i+=50000 ))
-# for (( i=1; i<=4; i+=50000 ))
-do
-    if [ $i -eq 1 ]; then
-    	let n=1
-    else
-    	let n=$i-1
-    fi
-
-    cmd="tau_f90.sh ${fflag} -DSETN=${n} -DSETRUN=10000 ${file}.F90 -o ${file}.exe"
-    run $cmd
-    run "./${file}.exe"
-    printf "${n};" >> ${output}
-    run "./extractInfoOMP.awk profile.0.0.0 ${output}"
-    run "rm -f ${file}.exe"
-done
-
+if [ -n "$file" ]; then
+    program
+else
+    echo "Stopping early, file needs to be set with -file=[name]"
+fi
 
 echo "FIN"
