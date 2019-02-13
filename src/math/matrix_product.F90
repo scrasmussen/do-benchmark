c0632ad2357732b7fb269a32581174a7185fc1d8
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-02-13 16:00:00 +0000

    better way to return variable

--- src/math/matrix_product.F90
+++ src/math/matrix_product.F90
@@ -1,9 +1,9 @@
 module matrix_product
 implicit none
 contains
-  function mat_vec_product_do(A,x,n,m)
+  function mat_vec_product_do(A,x,n,m) result(b)
     real, intent(IN) :: A(n,m), x(m)
-    real :: b(m), mat_vec_product_do(m)
+    real :: b(m)
     integer, intent(IN) :: n,m
     integer :: i,j
     do j=1,m
@@ -11,13 +11,12 @@ contains
           b(j) = b(j) + A(i,j) * x(i)
        end do
     end do
-    mat_vec_product_do=b
   end function mat_vec_product_do
 
-  function mat_vec_product_do_concurrent(A,x,n,m)
+  function mat_vec_product_do_concurrent(A,x,n,m) result(b)
     implicit none
     real, intent(IN) :: A(n,m), x(m)
-    real :: b(m), mat_vec_product_do_concurrent(m)
+    real :: b(m)
     integer, intent(IN) :: n,m
     integer :: i,j
     do concurrent (j=1:m)
@@ -25,14 +24,13 @@ contains
           b(j) = b(j) + A(i,j) * x(i)
        end do
     end do
-    mat_vec_product_do_concurrent=b
   end function mat_vec_product_do_concurrent
 
-  function mat_vec_product_omp(A,x,n,m)
+  function mat_vec_product_omp(A,x,n,m) result(b)
     use omp_lib
     implicit none
     real, intent(IN) :: A(n,m), x(m)
-    real :: b(m), mat_vec_product_omp(m)
+    real :: b(m)
     integer, intent(IN) :: n,m
     integer :: i,j
 !$OMP PARALLEL DO
@@ -41,14 +39,13 @@ contains
           b(j) = b(j) + A(i,j) * x(i)
        end do
     end do
-    mat_vec_product_omp=b
 !$OMP END PARALLEL DO
   end function mat_vec_product_omp
 
 
-  function mat_mat_product_do(A,B,n,k,m)
+  function mat_mat_product_do(A,B,n,k,m) result(C)
     real, intent(IN) :: A(n,k), B(k,m)
-    real :: mat_mat_product_do(n,m), C(n,m), sum
+    real :: C(n,m), sum
     integer, intent(IN) :: n,k,m
     integer :: i,j,z
     do j=1,m
@@ -60,11 +57,23 @@ contains
           C(i,j) = sum
        end do
     end do
-    mat_mat_product_do=C
   end function mat_mat_product_do
 
-  ! function mat_mat_product_do_concurrent
-  ! end function mat_mat_product_do_concurrent
+   function mat_mat_product_do_concurrent(A,B,n,k,m) result(C)
+    real, intent(IN) :: A(n,k), B(k,m)
+    real :: C(n,m), sum
+    integer, intent(IN) :: n,k,m
+    integer :: i,j,z
+    do concurrent (j=1:m)
+       do i=1,n
+          sum = 0
+          do z=1,k
+             sum = sum + A(i,z) * B(z,j)
+          end do
+          C(i,j) = sum
+       end do
+    end do
+  end function mat_mat_product_do_concurrent
 
   ! function mat_mat_product_omp
   ! end function mat_mat_product_omp
