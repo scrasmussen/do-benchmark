876a22ea1d54276a9ebdf6c5939f6580731b2785
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-10-31 04:15:14 -0500

    putting Do Concurrent in function so that it gets optimized

--- src/doConcurrent/dc3.F90
+++ src/doConcurrent/dc3.F90
@@ -11,13 +11,21 @@ program dc1
   report = .true.
 #endif
 
-  do concurrent(i=1:n, j=1:m, k=1:l)
-        A(i,j,k) = (i-1) + (j-1) * n + (k-1) * m * n
-  end do
+  call dc(A,n,m,l)
 
   if (report) then
      write(form,*) "(",n,"F5.0)"
      print form, A
   end if
 
+contains
+  subroutine dc(A, n, m, l)
+    implicit none
+    integer, intent(IN) :: n,m,l
+    real, intent(INOUT), dimension(n,m,l) :: A
+    integer :: i,j,k
+    do concurrent(i=1:n, j=1:m, k=1:l)
+       A(i,j,k) = (i-1) + (j-1) * n + (k-1) * m * n
+    end do
+  end subroutine dc
 end program dc1
